98aa8a4b96f53935e754276a3d3a219776b8eaef
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2026-01-18 22:03:20 +0100

    we up

---
+++ tests/performancetests/locustfile.py
@@ -0,0 +1,19 @@
+import random
+from locust import HttpUser, task, between
+
+class APIUser(HttpUser):
+    wait_time = between(1, 2)  # Simulate user waiting 1-2 seconds between tasks
+
+    @task(1)
+    def index(self):
+        self.client.get("/")
+
+    @task(3)
+    def predict(self):
+        # Generate random input for [Price, Load, Production] with sequence length of 168
+        dummy_data = [
+            [random.uniform(0, 100), random.uniform(100, 500), random.uniform(50, 200)] 
+            for _ in range(168)
+        ]
+        
+        self.client.post("/predict", json={"features": dummy_data})
